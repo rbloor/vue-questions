70fc49d436676f8f8ec805012e93d80bda80de41
Ryan Bloor <ryanbloor@me.com>
2020-05-29 22:18:42 +0100

    alter sitename, footer and build files

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -4,8 +4,8 @@
       <v-flex xs12>
         <div class="white--text text-center">
           Made with
-          <v-icon class="red--text">mdi-heart</v-icon>
-          by <a class="white--text" href="https://ryanbloor.co.uk" target="_blank">Ryan Bloor</a>
+          <v-icon class="red--text">mdi-heart</v-icon>by
+          <a class="white--text" href="https://ryanbloor.co.uk" target="_blank">Ryan Bloor</a>
         </div>
       </v-flex>
     </v-layout>
@@ -15,7 +15,7 @@
 <script>
 export default {
   name: "AppFooter"
-}
+};
 </script>
 
 <style scoped></style>
--- frontend/src/components/AppNavigation.vue
+++ frontend/src/components/AppNavigation.vue
@@ -3,36 +3,26 @@
     <v-navigation-drawer app v-model="drawer" class="indigo" dark disable-resize-watcher>
       <v-list>
         <v-list-item to="/">
-          <v-list-item-content>
-            Home
-          </v-list-item-content>
+          <v-list-item-content>Home</v-list-item-content>
         </v-list-item>
         <v-list-item :to="{ name: 'Login' }" v-if="!isLoggedIn">
-          <v-list-item-content>
-            Login
-          </v-list-item-content>
+          <v-list-item-content>Login</v-list-item-content>
         </v-list-item>
         <v-list-item :to="{ name: 'Register' }" v-if="!isLoggedIn">
-          <v-list-item-content>
-            Register
-          </v-list-item-content>
+          <v-list-item-content>Register</v-list-item-content>
         </v-list-item>
         <v-list-item :to="{ name: 'Dashboard' }" v-if="isLoggedIn">
-          <v-list-item-content>
-            Dashboard
-          </v-list-item-content>
+          <v-list-item-content>Dashboard</v-list-item-content>
         </v-list-item>
         <v-list-item @click="logout" v-if="isLoggedIn">
-          <v-list-item-content>
-            Logout
-          </v-list-item-content>
+          <v-list-item-content>Logout</v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
-      <v-toolbar-title>Recipe Book</v-toolbar-title>
+      <v-toolbar-title>Questions Frontend</v-toolbar-title>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
       <v-btn to="/" text class="hidden-sm-and-down">Home</v-btn>
       <v-btn :to="{ name: 'Login' }" v-if="!isLoggedIn" text class="hidden-sm-and-down">Login</v-btn>
@@ -44,7 +34,7 @@
 </template>
 
 <script>
-import User from "../apis/User"
+import User from "../apis/User";
 
 export default {
   name: "AppNavigation",
@@ -52,26 +42,26 @@ export default {
     return {
       drawer: false,
       isLoggedIn: false
-    }
+    };
   },
   mounted() {
     this.$root.$on("login", () => {
-      this.isLoggedIn = true
-    })
+      this.isLoggedIn = true;
+    });
 
-    this.isLoggedIn = !!localStorage.getItem("auth")
+    this.isLoggedIn = !!localStorage.getItem("auth");
   },
 
   methods: {
     logout() {
       User.logout().then(() => {
-        localStorage.removeItem("auth")
-        this.isLoggedIn = false
-        this.$router.push({ name: "Home" })
-      })
+        localStorage.removeItem("auth");
+        this.isLoggedIn = false;
+        this.$router.push({ name: "Home" });
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped></style>
